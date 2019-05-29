4f8b324adf1193beb3712513d505c6e25f8a9902
Matt D <dwinell.matt@gmail.com>
2019-05-29 14:51:17 -0400

    added initial sql file to create products table

---
+++ bamazon.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  id INT(5) NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NOT NULL,
+  department VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT(10) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department, price, stock_quantity)
+VALUES ("apple", "produce", 1.00, 100), ("banana", "produce", 0.2, 300), ("chair", "furniture", 80.00, 10), ("table", "furniture", 160, 20), ("Juggling_balls","miscellaneous", 5.50, 300), ("dog_whistle", "pets", 5.00, 100), ("milk_bath_powder", "cosmetics", 50, 5);
+SELECT * FROM products;
